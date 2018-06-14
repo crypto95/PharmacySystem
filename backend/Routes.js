a3e5497122d9e0189f423b10a036a5f9ee67a102
Shalindri <36409448+Shalindri@users.noreply.github.com>
2018-06-15 03:26:35 +0530

    changed main route

--- backend/Routes.js
+++ backend/Routes.js
@@ -6,9 +6,9 @@ var empRoutes = require("./Routers/Emp.Route");
 var issueRoutes = require("./Routers/Issue.Route");
 var paymentRoutes = require("./Routers/Payment.Route");
 
-routes.use("/drug", drugRoutes);
+//routes.use("/drug", drugRoutes);
 routes.use("/emp", empRoutes);
-routes.use("/issue", issueRoutes);
-routes.use("/payment", paymentRoutes);
+//routes.use("/issue", issueRoutes);
+//routes.use("/payment", paymentRoutes);
 
 module.exports = routes;
